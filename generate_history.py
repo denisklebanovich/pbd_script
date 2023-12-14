c1743850c40f75eec07347ecd1262de8ad0a7bd9
John Silver <cr1me7lavi@tutanota.com>
2023-12-14 02:19:27 +0100

    History

--- generate_history.py
+++ generate_history.py
@@ -1,45 +1,66 @@
 import random
+from datetime import timedelta, datetime
 
-from generate import cur, fake
+import psycopg2 as pg
+from faker import Faker
 
-application_status = {
-    "In progress": 0,
-    "Unpaid": 1,
-    "Examined": 2,
-    "Approved": 3,
-    "Rejected": 3
-}
+fake = Faker()
 
-cur.execute("SELECT pk_id FROM recruitment_applications")
-application_ids = cur.fetchall()
-
-
-for application_id in existing_application_ids:
-    # Ensure the first status is always "In progress"
-    status_sequence = ["In progress"]
-
-    # Some applications may be examined
-    if random.choice([True, False]):
-        status_sequence.append("Examined")
-
-        # Some applications may have an unpaid status
-        if random.choice([True, False]):
-            status_sequence.append("Unpaid")
-
-            # If examined, some applications may be approved or rejected
-            if "Examined" in status_sequence:
-                decision = random.choice(["Approved", "Rejected"])
-                status_sequence.append(decision)
-
-    date = fake.date_time_between(start_date="-5y", end_date="now")
-    # Loop through the status sequence for each application
-    for j, status in enumerate(status_sequence):
-        # Generate random timestamp for date_status_setting (replace with your logic)
-        random_days = random.randint(1, 5)
-        date = date + random_days
-
-        # Insert into applications_history
-        cur.execute('''
+INSERT_STATUS_QUERY = '''
             INSERT INTO applications_history (fk_application, fk_status, date_status_setting)
             VALUES (%s, %s, %s)
-        ''', (application_id, status, date_setting))
+        '''
+
+conn = pg.connect("dbname=recruitment user=postgres port=5432 password=admin")
+cur = conn.cursor()
+
+application_statuses = ["In progress", "Examined", "Unpaid", "Approved", "Rejected"]
+for status in application_statuses:
+    cur.execute('''
+        INSERT INTO application_status (pk_name)
+        VALUES (%s)
+    ''', (status,))
+
+cur.execute("SELECT pk_id,year FROM recruitment_applications")
+applications = cur.fetchall()
+
+
+# Define function to generate fake date within a range, with a maximum interval of 3 months
+def generate_fake_date_within_3_months(start_date, end_date):
+    max_interval = timedelta(days=90)  # 3 months
+    return fake.date_time_between_dates(
+        datetime(start_date, 1, 1),
+        min(datetime(end_date, 12, 31), datetime(start_date, 1, 1) + max_interval)
+    )
+
+
+# Insert fake data into applications_history
+for application_id, year in applications:
+
+    in_progress_date = generate_fake_date_within_3_months(year, year + 1)
+    statuses = ["In progress"]
+    last_status_date = in_progress_date
+    cur.execute(INSERT_STATUS_QUERY, (application_id, "In progress", in_progress_date))
+
+    examined = random.choices([True, False], weights=[0.2, 0.8])[0]
+    if year != 2023 or examined:
+        last_status_date = in_progress_date + timedelta(days=random.randint(1, 30 * 3))
+        cur.execute(INSERT_STATUS_QUERY,
+                    (application_id, "Examined", last_status_date))
+        statuses.append("Examined")
+
+    # "Unpaid" is optional
+    unpaid = random.choices([True, False], weights=[0.2, 0.8])[0]
+    if unpaid:
+        last_status_date = last_status_date + timedelta(days=random.randint(1, 30 * 3))
+        cur.execute(INSERT_STATUS_QUERY,
+                    (application_id, "Unpaid", last_status_date))
+
+    if (year == 2023 and random.choice([True, False]) and "Examined" in statuses) or year != 2023:
+        status = random.choice(["Approved", "Rejected"])
+        last_status_date = last_status_date + timedelta(days=random.randint(1, 30 * 3))
+        cur.execute(INSERT_STATUS_QUERY,
+                    (application_id, status, last_status_date))
+
+# Commit the changes
+conn.commit()
